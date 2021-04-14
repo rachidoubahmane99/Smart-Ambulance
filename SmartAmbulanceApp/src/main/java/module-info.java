27229a50aff5d20ac34d482989cb17369b8ea8ab
rachidoubahmane99 <rachidoubahman99@gmail.com>
2021-04-14 00:49:22 +0000

    create maven JavaFx app

---
+++ SmartAmbulanceApp/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.smartambulanceapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.smartambulanceapp to javafx.fxml;
+    exports com.mycompany.smartambulanceapp;
+}
+*/
